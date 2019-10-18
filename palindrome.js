e7e2b74ccd50f4afa6e1d40db83739ec2e90fb38
Maria Isabel Lopez <mrlpz1515@gmail.com>
2019-10-17 21:12:11 -0500

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,12 @@
 function palindrome(str) {
   var newstr = "";
   for (var i = 0; i<str.length;i++)
-    if (!str.charAt(i).match(/[^0-9a-z]/i))
-      newstr += str.charAt(i).toUpperCase();
+    if (!str[i].match(/[^0-9a-z]/i))
+      newstr += str[i].toUpperCase();
 
   for (var i = 0; i<newstr.length/2;i++){
-    if(newstr.charAt(i)!=newstr.charAt((newstr.length)-1-i)){
+    if(newstr[i].toUpperCase()!=newstr[newstr.length-1-i].toUpperCase())
       return false;
-    }
   }
   return true;
 }
