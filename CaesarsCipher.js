ddffc15c375ec93b1dc10dba41f4bf76ca5387a7
Maria Isabel Lopez <mrlpz1515@gmail.com>
2019-10-17 21:20:27 -0500

    Update CaesarsCipher.js

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -1,8 +1,8 @@
 function rot13(str) { // LBH QVQ VG!
   var code = "";
   for (var i=0; i<str.length; i++){
-    if (str.charAt(i).match(/[^A-Z]/))
-      code += str.charAt(i);
+    if (str[i].match(/[^A-Z]/))
+      code += str[i];
     else{
       var c = str.charCodeAt(i)+13;
       if (c>90) c = c - 26; 
