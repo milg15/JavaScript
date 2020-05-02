97f1584104dea1b899910424ab10ecf2dba43cff
Maria Isabel Lopez <mrlpz1515@gmail.com>
2020-05-02 15:50:52 -0500

    Update small_functions.js
    added more small functions. This are codes i made in the free code camp  Intermediate Algorithm Scripting section

--- small_functions.js
+++ small_functions.js
@@ -7,3 +7,59 @@ function diffArray(arr1, arr2) {
 }
 
 
+function destroyer(arr) {
+  let numbers = Array.from(arguments).slice(1)
+  return arr.filter(n => !numbers.includes(n))
+}
+
+
+//What's in a name
+function whatIsInAName(collection, source) {
+  // Only change code below this line
+  let sourceKey = Object.keys(source)
+  return collection
+         .filter(c => {
+            let exist = sourceKey
+             .filter(k => source[k] == c[k])
+             return exist.length == sourceKey.length
+            }
+          );
+}
+
+function whatIsInAName(collection, source) {
+  // Only change code below this line
+  let sourceKey = Object.keys(source)
+  return collection
+        .filter(c => 
+            sourceKey.every(k => c[k] == source[k]));
+}
+
+//splittt
+function spinalCase(str) {
+  return str.split(/(?=[A-Z])|\s|_|-/)
+        .join('-').toLowerCase();
+}
+
+//Pig latin -> interesting regex tool "exec" returns
+// index where it found the character
+function translatePigLatin(str) {
+  let hasVowel = /[aeiuo]/.exec(str);
+  let vowelIndex = (hasVowel) ? hasVowel.index : -1
+
+  return ( vowelIndex != 0 ? 
+        (str.substring(vowelIndex)
+        + str.substring(0, vowelIndex))
+          .concat("ay")
+    :   str.concat("way") )
+}
+
+//replace word and keep upper case
+function myReplace(str, before, after) {
+  //make first letter upper case 
+  after = (/^[A-Z]/.test(before)) 
+          ? after.charAt(0).toUpperCase() + after.slice(1) 
+          : after
+
+  return str.replace(before, after);
+}
+
