5be41e457d57a2c390d9b681e2000c99cbde0dca
Maria Isabel Lopez <mrlpz1515@gmail.com>
2020-05-02 12:48:05 -0500

    Update small_functions.js

--- small_functions.js
+++ small_functions.js
@@ -1,1 +1,9 @@
+function diffArray(arr1, arr2) {
+  let newArr1 = arr1.filter(elem => ! arr2.includes(elem));
+  let newArr2 = arr2.filter(elem => ! arr1.includes(elem));
+  
+  let newArr = newArr1.concat(newArr2)
+  return newArr;
+}
+
 
